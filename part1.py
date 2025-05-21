60b0fef449fd4355547d02dcd40e88e690eaa2db
Christin Estrella <estrella.cclm@gmail.com>
2025-05-21 09:50:21 +0800

    print list in alphabetical order

---
+++ part1.py
@@ -0,0 +1,11 @@
+# <!-- Please write a program which prints out an emoticon: :-) -->
+
+print(":-)")
+
+# "Seitsemän veljestä" is one of the first novels ever written in Finnish. It is the story of seven orphaned brothers learning to make their way in the world (read more on Wikipedia).
+
+# This program is supposed to print out the names of the brothers in alphabetical order, but it's not working quite right yet. Please fix the program so that the names are printed in the correct order.
+
+names =["Simeoni, Juhani", "Aapo", "Tuomas", "Eero", "Lauri", "Vilho", "Kalle"]
+names.sort()
+print(names)
