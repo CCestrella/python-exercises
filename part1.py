667a1073866858cda52a44bc2c5f4a9a5273e217
Christin Estrella <estrella.cclm@gmail.com>
2025-05-21 10:14:03 +0800

    Row, row, row your boat lyrics using newline characters

--- part1.py
+++ part1.py
@@ -6,6 +6,10 @@ print(":-)")
 
 # This program is supposed to print out the names of the brothers in alphabetical order, but it's not working quite right yet. Please fix the program so that the names are printed in the correct order.
 
-names =["Simeoni, Juhani", "Aapo", "Tuomas", "Eero", "Lauri", "Vilho", "Kalle"]
+names =["Simeoni", "Juhani", "Aapo", "Tuomas", "Eero", "Lauri", "Vilho", "Kalle"]
 names.sort()
 print(names)
+
+# Please write a program which prints out the following lines exactly as they are written here, punctuation and all:
+
+print("Row, row, row your boat,\nGently down the stream.\nMerrily, merrily, merrily, merrily,\nLife is but a dream.")
