8a84aa2687c86e7a86864d5866e8bba0ad2572d2
Christin Estrella <estrella.cclm@gmail.com>
2025-05-24 15:41:53 +0800

    typecasting

--- part2.py
+++ part2.py
@@ -5,3 +5,12 @@ if len(word) > 1:
     print(f"There are {len(word)} letters in the word {word}")
 
 print("thank you!")
+
+# Please write a program which asks the user for a floating point number and then prints out the integer part and the decimal part separately. Use the Python int function. /
+
+number = float(input("Please type in a number: "))
+
+print("The integer part is", int(number))
+
+decimal_part = number -int(number)
+print("The decimal part is", round(decimal_part, 2))
