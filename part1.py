09707659797c80ae0a79f9bb858330603dcbc933
Christin Estrella <estrella.cclm@gmail.com>
2025-05-31 14:58:07 +0800

    combining names

--- part1.py
+++ part1.py
@@ -6,20 +6,27 @@ print(":-)")
 
 # This program is supposed to print out the names of the brothers in alphabetical order, but it's not working quite right yet. Please fix the program so that the names are printed in the correct order.
 
-names =["Simeoni", "Juhani", "Aapo", "Tuomas", "Eero", "Lauri", "Vilho", "Kalle"]
-names.sort()
-print(names)
+# names =["Simeoni", "Juhani", "Aapo", "Tuomas", "Eero", "Lauri", "Vilho", "Kalle"]
+# names.sort()
+# print(names)
 
-# Please write a program which prints out the following lines exactly as they are written here, punctuation and all:
+# # Please write a program which prints out the following lines exactly as they are written here, punctuation and all:
 
-print("Row, row, row your boat,\nGently down the stream.\nMerrily, merrily, merrily, merrily,\nLife is but a dream.")
+# print("Row, row, row your boat,\nGently down the stream.\nMerrily, merrily, merrily, merrily,\nLife is but a dream.")
 
-# Please write a program which prints out the number of minutes in a year. Use Python code to perform the calculation, as in the previous code example.
+# # Please write a program which prints out the number of minutes in a year. Use Python code to perform the calculation, as in the previous code example.
 
-print("Minutes in a year:")
-print(365*24*60)
+# print("Minutes in a year:")
+# print(365*24*60)
 
-# Please write a program which prints out the following:
-# Sample output "Hello there!"
+# # Please write a program which prints out the following:
+# # Sample output "Hello there!"
 
-print('"Hello there!"')
+# print('"Hello there!"')
+
+given_name = "Beyonce"
+family_name = "Knowles"
+
+name = given_name + " " + family_name
+
+print(name)
