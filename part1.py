13874c31a587d91667abacd0441a463e0997c094
Christin Estrella <estrella.cclm@gmail.com>
2025-06-12 23:40:49 +0800

    boxing day

--- part1.py
+++ part1.py
@@ -24,9 +24,22 @@ print(":-)")
 
 # print('"Hello there!"')
 
-given_name = "Beyonce"
-family_name = "Knowles"
+# given_name = "Beyonce"
+# family_name = "Knowles"
 
-name = given_name + " " + family_name
+# name = given_name + " " + family_name
 
-print(name)
+# print(name)
+
+
+print("Holiday calendar")
+date = input("What is the date today? ")
+
+if date == "Dec 26":
+    print("It's Boxing Day")
+elif date == "Dec 31":
+    print("It's Hogmanay")
+elif date == "Jan 1":
+    print("It's New Year's Day")
+
+print("Thanks and bye.")
