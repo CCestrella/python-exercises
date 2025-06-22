e07af97c5b9b33f2451d94adc78d60c31a34b534
Christin Estrella <estrella.cclm@gmail.com>
2025-06-22 23:29:23 +0800

    fix the code: countdown

--- part3.py
+++ part3.py
@@ -9,3 +9,10 @@ print("Execution finished.")
 
 for number in range(2, 31, 2):
     print(number)
+
+print("Are you ready?")
+number = int(input("Please type in a number: "))
+while number > 0:
+    print(number)
+    number -= 1
+print("Now!")
