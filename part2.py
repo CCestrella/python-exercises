72b0145c96b352a1cbf82ac0c23b963fa66c3e58
Christin Estrella <estrella.cclm@gmail.com>
2025-06-22 23:26:13 +0800

    alphabetically in the middle

--- part2.py
+++ part2.py
@@ -258,4 +258,14 @@ elif number % 5 == 0:
 
 
 
+letter1 = input("1st letter: ")
+letter2 = input("2nd letter: ")
+letter3 = input("3rd letter: ")
+
+letters = [letter1, letter2, letter3]
+
+letters.sort()
+
+print("The letter in the middle is", letters[1])
+
       
