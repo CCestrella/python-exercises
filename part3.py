e5e3549ee539da2c85d23909614c655732fa2f3f
Christin Estrella <estrella.cclm@gmail.com>
2025-06-25 22:21:29 +0800

    consecutive numbers

--- part3.py
+++ part3.py
@@ -38,3 +38,13 @@ number = 1
 while number <= limit:
     print(number)
     number *= base
+
+limit = int(input("Limit: "))
+total = 0
+number = 1
+
+while total < limit:
+    total += number
+    number += 1
+
+print(total)
